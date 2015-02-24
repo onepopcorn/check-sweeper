918dde264843fdef7f8e0583e4507d6f9430682e
Marc Feliu <mfeliu@adncomunicacio.com>
2015-02-24 18:04:54 +0100

    Modified gulp tasks to prevent pipe break

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp   = require('gulp'),
 	html   = require('gulp-htmlhint'),
 	sass   = require('gulp-sass'),
 	clean  = require('gulp-clean'),
-	sync   = require('browser-sync');
+	sync   = require('browser-sync'),
+	plumber= require('gulp-plumber'),
+	colors = require('colors');
 
 var paths = {
 	scripts : 'src/js/**/*.js',
@@ -12,6 +14,13 @@ var paths = {
 	html    : 'src/*.html',
 	images  : ['src/img/**/*.png','src/img/**/*.jpg','src/img/**/*.gif']
 }
+
+///////// Error handling /////////
+function errorHanlder(err){
+	console.error('['+'error:'.red+']',err.message);
+	console.error('['+'line:'.red+']',err.lineNumber);
+	console.log('['+'file:'.red+']', err.fileName);
+};
 
 
 ///////// Cleaning tasks /////////
@@ -33,20 +42,27 @@ gulp.task('styles-clean',function(){
 ///////// Main tasks /////////
 gulp.task('markup',['markup-clean'],function(){
 	return gulp.src(paths.html)
+		   .pipe(plumber())
 		   .pipe(html())
+		   .on('error', errorHanlder)
 		   .pipe(gulp.dest('bin'));
 });
 
 gulp.task('scripts',['scripts-clean'],function(){
 	return gulp.src(paths.scripts)
+		   .pipe(plumber())
 		   .pipe(jshint())
+		   .on('error', errorHanlder)
 		   .pipe(uglify())
+		   .on('error', errorHanlder)
 		   .pipe(gulp.dest('bin/js'));
 });
 
 gulp.task('styles',['styles-clean'],function(){
 	return gulp.src(paths.styles)
+		   .pipe(plumber())
 		   .pipe(sass())
+		   .on('error', errorHanlder)
 		   .pipe(gulp.dest('bin/css'));
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 (function(){
+	// Caché the board element
+	var board = document.querySelector('#gameboard');
 
+	// 
 	var revealCheckbox = function(e){
 		e.preventDefault();
 		e.currentTarget.className = 'hidden';
@@ -13,10 +16,16 @@
 	};
 
 	var all = document.querySelectorAll('#gameboard>.tile>input');
-	
-	for(var i=0;var len = all.length;i<len;i++)
+	for(var i=0, len = all.length;i<len;i++)
 	{
 		all[i].addEventListener('contextmenu',markCheckbox);
 		all[i].addEventListener('click',revealCheckbox);	
-	}
+	};
+	var getCell = function(row,col){
+		var el = board.children[row * col];
+		return el;
+	};
+	var tile = getCell(0,5);
+	console.log(tile);
+
 })();
